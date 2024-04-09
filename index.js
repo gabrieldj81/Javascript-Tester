a310e18fef048656bae2adafe16eeb32d01cdf7d
Gabrieldj81 <130005291+gabrieldj81@users.noreply.github.com>
2024-04-08 20:26:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+alert("Mad Libs by Cake Logic");
+alert("At the end, please rate me a 5/5. It took me a lot of work to make this.");
+let story;
+story = prompt("Pick A Story: 1 or 2");
+if(story == "1"){
+let adj1 = prompt("1/41- Name an Adjetive.");
+let noun1 = prompt("2/41- Name a noun.");
+let adj2 = prompt("3/41- Name an Adjective.");
+let name = prompt("4/41- Give the main character a name.");
+let adj3 = prompt("5/41- Name an Adjective.");
+let verb1 = prompt("6/41- Name a Verb.(Present Tense)");
+let adj4 = prompt("7/41- Name an Adjective.");
+let verb2 = prompt("8/41- Name an Verb.(Present Tense)");
+let noun2 = prompt("9/41- Name a Noun.");
+let noun3 = prompt("10/41- Name a Noun.");
+let thh = prompt("11/41- Choose one: Them, Him, or Her.");
+let diauloge = prompt("12/41- What should the Character Say?(Diauloge)");
+let noun4 = prompt("13/41- Name a Noun.");
+let noun5 = prompt("14/41- Name a Noun.(Is This a glitch? No.)");
+let noun6 = prompt("15/41- Name a Noun.(No this is not a glitch, I need a noun.)");
+let adj5 = prompt("16/51- Name an Adjective.");
+let noun7 = prompt("17/41- Name a Noun.");
+let adj6 = prompt("18/41- Name an Adjective.");
+let noun8 = prompt("19/41- Name a Noun.");
+let adj7 = prompt("20/41- Name an Adjective.");
+let noun9 = prompt("21/41- Name a Noun.");
+let verb3 = prompt("22/41- Name a Verb.(Present Tense)");
+let noun10 = prompt("23/41- Name a Noun.");
+let adverb = prompt("24/41- Name an Adverb.");
+let noun11 = prompt("25/41- Name a Noun.");
+let noun12 = prompt("26/41- Name a Noun.");
+let adj8 = prompt("27/41- Name an Adjective");
+let noun13 = prompt("28/41- Name a Noun.");
+let verb4 = prompt("29/41- Name a Verb.(Present Tense)");
+let noun14 = prompt("30/41- Name a Noun.");
+let verb5 = prompt("31/41- Name a Verb.(Past Tense)");
+let time = prompt("32/41- Specify a Time.");
+let noun15 = prompt("33/41- Name a Noun.");
+let verb6 = prompt("34/41- Name a Noun.");
+let direct = prompt("35/41- Name a Direction.");
+let verb7 = prompt("36/41- Name a Verb.(Past Tense)");
+let verb8 = prompt("37/41- Name a Verb.(Past Tense)");
+let verb9 = prompt("38/41- Name a Verb.(Past Tense)");
+let verb10 = prompt("39/41- Name a Noun.");
+let verb11 = prompt("40/41- Name a Verb.(Past Tense)");
+let adj9 = prompt("41/41- Name an Adjective");
+alert("And You Finished! Let's go see that story your wrote!");
+alert("Ahem...");
+alert("Today's Story is Once Upon an " + noun1);
+alert("In the" +  " " +  adj1 + " " + "town of" + " " + noun1 + " " + "land,");
+alert("there was a" + " " +  adj2 + " " +  "Princess,");
+alert("who's name was" + " " + name + ".");
+alert("There was also an absolutley" + " " +  adj3 + " " + "witch, who would" + " " + verb1 + " " + "anyone who crossed their path." );
+alert("One day, the princess needed to find a" + " "+  adj4 + " " + "dress to the" + " " + verb2 + " " + "to" + " "+ verb3 +" everyone at the" + " " + noun2 + ".");
+alert("So the animals of the" + " " + noun3 + " " + "decided to help" + " " + thh + ".");
+alert("When the princess saw the dress, they yelled out" + " " + "'" + diauloge + "'"+  " " + "and discarded the" + " " + noun3 + ".");
+alert("All of the sudden, a fairy" + " " + noun4 + " " + "came from the" + " " + noun5 + " " + "and helped the princess get a" + " " + adj5 + " " + noun6 + ".");
+alert("The" + " " + adj6 + " " + "dressed princess, arrived at the ball in a" + " " + noun7 + " " +  "that the fairy" + " " + noun8 + " " + "gave them.");
+alert("The ball was so" + " " + adj7 + " " + "it made her" + " " + noun9 + " " + verb4 + " " + "at the sight of it.");
+alert("The witch saw the princess at the" + " " + noun10 + " and " +  adverb + " " + "put a pouisnous" + " " + noun11 + " " + "on the snack" + " " + noun12 + ".");
+alert("The princess saw the" + " " + adj8 + " " + noun13 + " " + "and went to go" + " " + verb5 + " it.");
+alert("As soon as they did that, they were poisouned.");
+alert("The only thing that could cure it would be a " +  noun14 + " " + "from a " + verb6 + ".");
+alert("The " + noun15 + " had arrived at the ball and saw the princess was" + " " + verb7 + ".");
+alert("Because he watched the entierety of Snow White before he went to the ball, he knew exactly what to do.");
+alert("He knelt " + direct + " and " + verb8 + " the princess, and then they were" + " " + verb9 + ".");
+alert("The Princess and the " + verb10 + " went on to become " + verb11 + " and live a " + adj9 + " life.");
+let useless = prompt("On a scale of 1-5, how do you rate the Mad Lib?");
+ if(useless <  3){
+    alert("You just love making people feel ashamed!")
+   }
+   if(useless > 3){
+    alert("How Nice of You!");
+   }
+}
+if(story == "2"){
+let adjone = prompt("1/13 Name an Adjetive.");
+let place1 = prompt("2/13 Name a Place.");
+let adjtwo = prompt("3/13 Name an Adjetive.");
+let adjthree = prompt("4/13 Name an Adjetive.");
+let poc = prompt("5/13 Name a peice of clothing.");
+let bodyp1 = prompt("6/13 Name a body part.");
+let bodyp2 = prompt("7/13 Name a body part.");
+let bodyp3 = prompt("8/13 Name a body part.");
+let adjfour = prompt("9/13 Name an Adjetive.");
+let nounone = prompt("10/13 Name a Noun.");
+let nountwo = prompt("11/13 Name a Noun.");
+let place2 = prompt("12/13 Name a Place.");
+let adjfive = prompt("13/13 Name an Adjetive.");
+alert("And you Finished! Let's see that story you wrote!");
+alert("Ahem...");
+alert("Today's story is: Going to the Doctor.");
+alert("Every year, you should go visit the doctor.");
+alert("It is a very " + adjone + " visit.");
+alert("Usually, you have to skip going to " + place1 + " to go.");
+alert("Your doctor is usually a/an " + adjtwo + " man or woman who is wearing " + adjthree + " " + poc + ".");
+alert("They will look at your " + bodyp1 + " " + bodyp2 + " ,and " + bodyp3 + ".");
+alert("Sometimes, they can be very " + adjfour + ".");
+alert("Afterwards, they will give you a " + nounone + " and a " + nountwo + " and your mom or dad will take you to " + place2 + " as a treat.");
+alert("All in all, the doctor's office isn't so " + adjfive + "!");
+let useless2 = prompt("On a scale of 1-5, how do you rate the mad lib?");
+ if(useless2 <  3){
+    alert("You just love making people feel ashamed!")
+   }
+   if(useless2 > 3){
+    alert("How Nice of You!");
+   }
+
+}
